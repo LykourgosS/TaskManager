c0648e8685d19e32f071a6c636ccf3481887d1b7
LykourgosS <lpsarantidis@gmail.com>
2024-02-12 20:34:54 +0200

    BugFix use class name (i.e. Task) to refer to static enum Status

--- modules/Task.js
+++ modules/Task.js
@@ -2,9 +2,9 @@ export class Task {
     constructor(title, description) {
         this.title = title
         this.description = description
-        this.status = Status.Incomplete
+        this.status = Task.Status.Incomplete
     }
-
+    
     static Status = {
         Incomplete: 0,
         Completed: 1
